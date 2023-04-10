aa99e1f5be8b16ff103c04184e48ec57c60a7f79
Женя Шульман <jonnie.gray@yandex.ru>
2023-04-10 19:40:38 +0500

    Sprint9OVER

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,7 +7,7 @@ from .views import (CommentViewSet, GroupViewSet,
 router_v1 = DefaultRouter()
 
 router_v1.register('posts', PostViewSet, basename='posts')
-router_v1.register(r'posts/(?P<post_id>\\d+)/comments', CommentViewSet,
+router_v1.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet,
                    basename='comments')
 router_v1.register('groups', GroupViewSet, basename='groups')
 router_v1.register('follow', FollowViewSet, basename='follow')
